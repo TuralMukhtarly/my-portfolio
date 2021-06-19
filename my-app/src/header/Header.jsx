8ca0e90a883f2d3eae08411abcf1f5a31077df5d
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-06-20 00:00:38 +0300

    5 commit

--- my-app/src/header/Header.jsx
+++ my-app/src/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Nav from '../nav/Nav'
+import style from './Header.module.css'
 
 function Header() {
     return (
-        <div>
+        <div className={style.headerBlock}>
             <Nav/>
         </div>
     )
--- my-app/src/main/Main.jsx
+++ my-app/src/main/Main.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import style from './Main.module.css'
 import styleContainer from '../common/styles/Container.module.css'
-import Nav from '../nav/Nav'
 
 function Main() {
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
+
+                <div className={style.text}>
+                    <span>Hi</span>
+                    <h1>I'am Tural</h1>
+                    <p>FrontEnd Developer</p>
+                </div>
+
                 <div className={style.photo}>
 
                 </div>
-                <div className={style.text}>
-                    <h1>I'AM TURAL MUKHTARLY</h1>
-                    <h2>Front-End Developer</h2>
-                    <p>I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
-                    <button className={style.btn}>MORE ABOUT ME</button>
-                </div>
-
-                <Nav />
             </div>
         </div>
     )
